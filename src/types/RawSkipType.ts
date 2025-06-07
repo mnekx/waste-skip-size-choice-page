2fcb56180efd9d22ea9e60a237b7b8af0eb493ba
Mnemba Chambuya <mnemba.chambuya@gmail.com>
2025-06-08 01:24:21 +0300

    default filter state, raw skip type

---
+++ src/types/RawSkipType.ts
@@ -0,0 +1,15 @@
+export type RawSkipType = {
+	size: number;
+	hire_period_days: number;
+	transport_cost: number;
+	per_tonne_cost: number;
+	price_before_vat: number;
+	vat: number;
+	postcode: string;
+	area: string;
+	forbidden: boolean;
+	created_at: string;
+	updated_at: string;
+	allowed_on_road: boolean;
+	allows_heavy_waste: boolean;
+};
